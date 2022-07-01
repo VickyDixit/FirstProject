22e3d568b507d42431ae42f65b558111e29b6ad7
VickyDixit <dixitvicky521@gmail.com>
2022-07-01 09:27:53 +0530

    done

---
+++ JavaScript_Modal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JavaScript_Modal.Startup))]
+namespace JavaScript_Modal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
